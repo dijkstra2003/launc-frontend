532e3f7928ead086ada11ec0d550ba9e9362fd58
Joey de Ruiter <contact@joeyderuiter.me>
2019-11-14 13:37:10 +0100

    Always tag the latest build with latest, except the release build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,13 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t launc --build-arg API_ROUTE=$2 .
 docker images
-docker tag launc:latest $DOCKER_USERNAME/launc-frontend:$1
+
+if [ $1 == "release" ]; then
+    docker tag launc:latest $DOCKER_USERNAME/launc-frontend:$1
+else
+    docker tag launc:latest $DOCKER_USERNAME/launc-frontend:latest
+    docker tag launc:latest $DOCKER_USERNAME/launc-frontend:$1
+fi
+
 docker push $DOCKER_USERNAME/launc-frontend
 
