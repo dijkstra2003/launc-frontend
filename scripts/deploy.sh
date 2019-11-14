7411d5f97d25fce546de7f9955cb4e6ee9876665
Joey de Ruiter <contact@joeyderuiter.me>
2019-11-14 10:04:21 +0100

    Changed Docker tagging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t launc --build-arg API_ROUTE=$2 .
 docker images
-docker tag launc:$1 $DOCKER_USERNAME/launc-frontend
+docker tag launc:latest $DOCKER_USERNAME/launc-frontend:$1
 docker push $DOCKER_USERNAME/launc-frontend
 
