e1a72e5f0f3e467ab9a06d683213513806dae245
Joshua Tromp <jtr@netcompany.com>
2019-11-26 10:37:54 +0100

    login works

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username, password) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+      })
+    };
+    this.http.post('http://localhost:20450/users/authenticate', {username, password}, httpOptions).subscribe(e => {
+      console.log(e);
+    }, err => {
+      console.log(err);
+    });
+  }
 
   ngOnInit() {
     document.querySelectorAll('.js-target').forEach((button) => {
