3c1a6e8eb1b489cd7b9972b30c0b451c88625015
Luc Dijsktra <dijkstra2003@gmail.com>
2019-12-17 13:43:43 +0100

    tests fix

--- src/app/components/campaigns-page/campaigns-page.component.spec.ts
+++ src/app/components/campaigns-page/campaigns-page.component.spec.ts
@@ -16,6 +16,7 @@ describe('CampaignsPageComponent', () => {
       ],
       declarations: [
         CampaignsPageComponent,
+        CampaignCardComponent
       ]
     })
     .compileComponents();
