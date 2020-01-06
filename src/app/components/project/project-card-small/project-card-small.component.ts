db8bacf68f0318088cf31cd4026b0e7b6f379b3f
DenNie <denzel_nievaart@hotmail.nl>
2020-01-06 12:54:24 +0100

    Home page displaying list of 6 projects

--- src/app/components/project/project-card-small/project-card-small.component.ts
+++ src/app/components/project/project-card-small/project-card-small.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Campaign } from 'src/app/models/Campaign';
 
 @Component({
   selector: 'app-project-card-small',
@@ -6,9 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./project-card-small.component.scss']
 })
 export class ProjectCardSmallComponent implements OnInit {
-  @Input() title: string;
-  @Input() description: string;
-  @Input() logoSrc: string;
+  @Input() selectedCampaign = {} as Campaign;
 
   constructor() { }
 
