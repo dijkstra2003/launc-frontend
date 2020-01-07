fc749519cbd0e4538046e985a043443d22b0793a
Luc Dijsktra <dijkstra2003@gmail.com>
2020-01-07 11:37:00 +0100

    payment works

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -14,12 +14,25 @@ export class PaymentComponent implements OnInit {
   }
 
   startPayment(euros, cents, method) {
+    if(euros.length === 0 && cents.length === 0) {
+      euros = 10;
+      cents = 0;
+    }
+
+    if(cents.length === 0 && euros.length !== 0) {
+      cents = 0;
+    }
+
+    if(euros.length === 0 && cents.length !== 0) {
+      euros = 0;
+    }
+
     var goalId = parseInt(sessionStorage.getItem("selectedCampaign"));
     var amount = (parseInt(euros) + (cents / 100)).toFixed(2);
     method = method.toLowerCase();
     
-    this.paymentService.initiatePayment(amount, goalId, method).subscribe(success => {
-      console.log(success);
+    this.paymentService.initiatePayment(goalId, amount, method).subscribe(success => {
+      window.location.href = success['url'];
     },
     error => {
       console.log(error);
