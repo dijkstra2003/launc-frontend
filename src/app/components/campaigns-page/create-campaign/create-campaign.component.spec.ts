5f8ac7be80cb38477162597037df9bb74fab09cf
Joshua Tromp <jtr@netcompany.com>
2020-01-07 10:28:09 +0100

    fixed errors

--- src/app/components/campaigns-page/create-campaign/create-campaign.component.spec.ts
+++ src/app/components/campaigns-page/create-campaign/create-campaign.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateCampaignComponent } from './create-campaign.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/modules/material.module';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepicker, MatDatepickerModule } from '@angular/material/datepicker';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CreateCampaignComponent', () => {
   let component: CreateCampaignComponent;
@@ -8,7 +16,15 @@ describe('CreateCampaignComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateCampaignComponent ]
+      declarations: [ CreateCampaignComponent ],
+      imports: [
+        ReactiveFormsModule,
+        MatNativeDateModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        BrowserAnimationsModule,
+        HttpClientModule]
     })
     .compileComponents();
   }));
